c1a5b852c5a3f34818df1bf66e812c16774039ca
Cris <desarrollo2.tic@proing.com.co>
2021-06-01 07:13:47 -0500

    JS Orientado a Objetos ES6

--- JS/index.js
+++ JS/index.js
@@ -1,3 +1,3 @@
-var car = new Car("CDO17A", new Account("Cristhian Vargas", "11441231231"));
+var car = new Car("CDO17A", new Account("Cristhian Vargas", "114412231"));
 car.passenger = 4;
 car.printDataCar();
