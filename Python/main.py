bc0975c11ab56636782e6c0491e977b4c667653c
Cris <desarrollo2.tic@proing.com.co>
2021-06-01 19:13:06 -0500

    Contructor python

--- Python/main.py
+++ Python/main.py
@@ -1,2 +1,14 @@
+from car import Car
+from account import Account
+
 if __name__ == "__main__":
     print("Hola mundo")
+    car = Car("EGH41D", Account("cristhian vargas", "1144123324"))
+    print(vars(car))
+    print(vars(car.driver))
+
+    # car2 = Car()
+    # car2.licence = 55
+    # car2.driver = "Zonia Vargas"
+    # print(vars(car2))
+    
