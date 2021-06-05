b69ea740df565e86df120834e1fcdccc4e2473ac
Cris <desarrollo2.tic@proing.com.co>
2021-06-05 11:06:21 -0500

    Herencia Python

--- Python/main.py
+++ Python/main.py
@@ -1,12 +1,15 @@
 from car import Car
 from account import Account
+from uberBlack import UberBlack
 
 if __name__ == "__main__":
-    print("Hola mundo")
+    # print("Hola mundo")
     car = Car("EGH41D", Account("cristhian vargas", "1144123324"))
-    print(vars(car))
-    print(vars(car.driver))
+    # print(vars(car))
+    # print(vars(car.driver))
 
+    uberBlack = UberBlack("EGH-41D", Account("Valentino Rossi", "75675455"), "F1", ["Cuero", 'Gamuza'])
+    print(vars(uberBlack))
     # car2 = Car()
     # car2.licence = 55
     # car2.driver = "Zonia Vargas"
