0166c9e9ecbd6b485e715d8a9192d2bf1229181f
Cris <desarrollo2.tic@proing.com.co>
2021-06-07 11:30:53 -0500

    User y Driver heredan de Acocunt

---
+++ PHP/User.php
@@ -0,0 +1,10 @@
+<?php
+require_once('Account.php');
+
+class User extends Account {
+
+    function __construct($name, $document) {
+        parent::__construct($name, $document);
+        
+    }
+}
